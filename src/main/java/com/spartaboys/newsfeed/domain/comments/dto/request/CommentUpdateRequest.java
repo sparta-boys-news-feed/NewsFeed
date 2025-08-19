7a1d1271999ab28baa171ad08168cd1f248881da
donghyeon505 <ehdgus458@gmail.com>
2025-08-19 21:00:11 +0900

    refactor(comments) : @RequiredArgsConstructor 사용으로 코드 리팩토링

--- src/main/java/com/spartaboys/newsfeed/domain/comments/dto/request/CommentUpdateRequest.java
+++ src/main/java/com/spartaboys/newsfeed/domain/comments/dto/request/CommentUpdateRequest.java
@@ -3,15 +3,13 @@ package com.spartaboys.newsfeed.domain.comments.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class CommentUpdateRequest {
 
     @NotBlank(message = "댓글 내용은 필수입니다.")
     @Size(min = 2, max = 100, message = "댓글 내용은 2자 이상 100자 이하입니다.")
     private final String content;
-
-    public CommentUpdateRequest(String content) {
-        this.content = content;
-    }
 }
