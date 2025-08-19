4a6fe97629387aabd5dbe4b7875467c5d58cba4a
donghyeon505 <ehdgus458@gmail.com>
2025-08-19 21:01:23 +0900

    refactor(comments) : Mapper 클래스 변환 메서드 Builder 방식으로 통일

--- src/main/java/com/spartaboys/newsfeed/domain/comments/dto/response/CommentGetAllResponse.java
+++ src/main/java/com/spartaboys/newsfeed/domain/comments/dto/response/CommentGetAllResponse.java
@@ -5,16 +5,18 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Builder
 @AllArgsConstructor
-public class CommentResponse {
+public class CommentGetAllResponse {
 
     private final Long id;
     private final String content;
     private final String nickname;
     private final int likes;
+    private final List<CommentResponse> replies;
     private final LocalDateTime createdAt;
     private final LocalDateTime updatedAt;
 }
