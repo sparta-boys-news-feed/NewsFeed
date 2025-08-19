cd006f7b98da88895a6eef66819b4e62ef91d3fa
Over-night <hidden98716@gmail.com>
2025-08-19 11:27:14 +0900

    feat: User Entity & Repository Upload

--- src/main/java/com/spartaboys/newsfeed/domain/users/repository/UserRepository.java
+++ src/main/java/com/spartaboys/newsfeed/domain/users/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package com.spartaboys.newsfeed.domain.users.repository;
 
+import com.spartaboys.newsfeed.domain.users.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByNickname(String username);
+    List<User> findByNicknameContaining(String nickname);
+
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String username);
 }
