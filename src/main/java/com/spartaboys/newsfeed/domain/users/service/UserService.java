fffe313d496124c60468519bbbc24d49b110f734
Over-night <hidden98716@gmail.com>
2025-08-19 19:34:21 +0900

    feat: dtorepository 갱신, d가 및 Service & 갱신

--- src/main/java/com/spartaboys/newsfeed/domain/users/service/UserService.java
+++ src/main/java/com/spartaboys/newsfeed/domain/users/service/UserService.java
@@ -1,4 +1,17 @@
 package com.spartaboys.newsfeed.domain.users.service;
 
+import com.spartaboys.newsfeed.domain.users.dto.response.UserPublicResponse;
+import com.spartaboys.newsfeed.domain.users.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
+    private UserRepository userRepository;
+
+    private UserPublicResponse getUserById
+
 }
