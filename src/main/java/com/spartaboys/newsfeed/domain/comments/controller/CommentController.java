7f121d4ebd3bd4b907ba7f3105983c315a43d6c2
donghyeon505 <ehdgus458@gmail.com>
2025-08-20 20:08:35 +0900

    feat(comments) : 대댓글 CR 추가 - Comment 랩퍼 메서드 추가

--- src/main/java/com/spartaboys/newsfeed/domain/comments/controller/CommentController.java
+++ src/main/java/com/spartaboys/newsfeed/domain/comments/controller/CommentController.java
@@ -4,6 +4,7 @@ import com.spartaboys.newsfeed.common.response.ApiPageResponse;
 import com.spartaboys.newsfeed.common.response.ApiResponse;
 import com.spartaboys.newsfeed.domain.comments.dto.request.CommentCreateRequest;
 import com.spartaboys.newsfeed.domain.comments.dto.request.CommentUpdateRequest;
+import com.spartaboys.newsfeed.domain.comments.dto.response.CommentGetAllResponse;
 import com.spartaboys.newsfeed.domain.comments.dto.response.CommentResponse;
 import com.spartaboys.newsfeed.domain.comments.service.CommentService;
 import com.spartaboys.newsfeed.domain.users.entity.User;
@@ -22,20 +23,21 @@ public class CommentController {
 
     private final CommentService commentService;
 
-    @PostMapping
+    @PostMapping("/{commentId}")
     public ResponseEntity<ApiResponse<CommentResponse>> createComment(
             @PathVariable Long boardId,
+            @PathVariable Long commentId,
             @SessionAttribute("user") User loginUser,
             @Valid @RequestBody CommentCreateRequest commentRequest
     ) {
 
-        CommentResponse response = commentService.createComment(boardId, loginUser, commentRequest);
+        CommentResponse response = commentService.createComment(boardId, commentId, loginUser, commentRequest);
 
         return ApiResponse.created(response);
     }
 
     @GetMapping
-    public ResponseEntity<ApiPageResponse<CommentResponse>> getAllByBoardId(
+    public ResponseEntity<ApiPageResponse<CommentGetAllResponse>> getAllByBoardId(
             @PathVariable Long boardId,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "20") int size
@@ -43,7 +45,7 @@ public class CommentController {
         // 페이지 객체 생성
         Pageable pageable = PageRequest.of(page, size);
 
-        Page<CommentResponse> response = commentService.getAllByBoardId(boardId, pageable);
+        Page<CommentGetAllResponse> response = commentService.getAllByBoardId(boardId, pageable);
 
         return ApiPageResponse.success(response);
     }
--- src/main/java/com/spartaboys/newsfeed/domain/comments/repository/CommentRepository.java
+++ src/main/java/com/spartaboys/newsfeed/domain/comments/repository/CommentRepository.java
@@ -8,10 +8,17 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    // 부모 댓글 찾아서 ( ParentComment 가 Null ) 생성일 기준으로 내림차순
     @EntityGraph(attributePaths = "user")
-    Page<Comment> findAllByBoardIdOrderByCreatedAtDesc(Long boardId, Pageable pageable);
+    Page<Comment> findByBoardIdAndParentCommentIsNullOrderByCreatedAtDesc(Long boardId, Pageable pageable);
+
+    // 대댓글 찾기 ( ParentComment 가 Not Null )
+    @EntityGraph(attributePaths = "user")
+    List<Comment> findByBoardIdAndParentCommentIsNotNull(Long boardId);
 
     default Comment findByIdOrThrowElse (Long commentId) {
         return findById(commentId).orElseThrow(() -> new InvalidCommentException(CommentErrorCode.COMMENT_NOT_FOUND));
