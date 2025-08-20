e81be1e902ec1074da6a89bf77fac5dcfe5c5d62
donghyeon505 <ehdgus458@gmail.com>
2025-08-20 20:08:35 +0900

    feat(comments) : 대댓글 CR 추가 - Comment 랩퍼 메서드 추가

--- src/main/java/com/spartaboys/newsfeed/domain/comments/dto/response/CommentGetAllResponse.java
+++ src/main/java/com/spartaboys/newsfeed/domain/comments/dto/response/CommentGetAllResponse.java
@@ -1,6 +1,5 @@
 package com.spartaboys.newsfeed.domain.comments.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,14 +8,13 @@ import java.util.List;
 
 @Getter
 @Builder
-@AllArgsConstructor
 public class CommentGetAllResponse {
 
     private final Long id;
     private final String content;
     private final String nickname;
     private final int likes;
-    private final List<CommentResponse> replies;
     private final LocalDateTime createdAt;
     private final LocalDateTime updatedAt;
+    private final List<CommentResponse> replies;
 }
