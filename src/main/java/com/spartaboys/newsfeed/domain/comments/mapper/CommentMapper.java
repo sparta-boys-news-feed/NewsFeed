3eb3d6ebe92d20789aaf796e723e972971c8d3ca
donghyeon505 <ehdgus458@gmail.com>
2025-08-20 20:08:35 +0900

    feat(comments) : 대댓글 CR 추가 - Comment 랩퍼 메서드 추가

--- src/main/java/com/spartaboys/newsfeed/domain/comments/mapper/CommentMapper.java
+++ src/main/java/com/spartaboys/newsfeed/domain/comments/mapper/CommentMapper.java
@@ -2,38 +2,55 @@ package com.spartaboys.newsfeed.domain.comments.mapper;
 
 import com.spartaboys.newsfeed.domain.boards.entity.Board;
 import com.spartaboys.newsfeed.domain.comments.dto.request.CommentCreateRequest;
+import com.spartaboys.newsfeed.domain.comments.dto.response.CommentGetAllResponse;
 import com.spartaboys.newsfeed.domain.comments.dto.response.CommentResponse;
 import com.spartaboys.newsfeed.domain.comments.entity.Comment;
 import com.spartaboys.newsfeed.domain.users.entity.User;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class CommentMapper {
 
     // CommentCreateRequest DTO를 Comment 엔티티로 변환
-    public Comment toEntity(CommentCreateRequest request, Board board, User user) {
+    public Comment toEntity(CommentCreateRequest request, Board board, User user, Comment parentComment) {
         return Comment.builder()
                 .content(request.getContent())
                 .user(user)
                 .board(board)
+                .parentComment(parentComment)
                 .build();
     }
 
-    // Comment 엔티티를 DTO로 변환
-    public CommentResponse toDto(Comment comment) {
-        return CommentResponse.builder()
-                .id(comment.getId())
-                .content(comment.getContent())
-                .nickname(comment.getUser().getNickname())
-                .likes(comment.getLikes())
-                .createdAt(comment.getCreatedAt())
-                .updatedAt(comment.getUpdatedAt())
+    // 부모 댓글 -> DTO (대댓글 리스트 포함)
+    public CommentGetAllResponse toDto(Comment parent, List<Comment> replies) {
+        return CommentGetAllResponse.builder()
+                .id(parent.getId())
+                .content(parent.getContent())
+                .nickname(parent.getUser().getNickname())
+                .likes(parent.getLikes())
+                .createdAt(parent.getCreatedAt())
+                .updatedAt(parent.getUpdatedAt())
+                .replies(toDto(replies)) // 대댓글 변환
                 .build();
     }
 
-    // 댓글 페이징 Comment을 DTO로 변환
-    public Page<CommentResponse> toDto(Page<Comment> comments) {
-        return comments.map(this::toDto);
+    // 대댓글 & 단일 댓글 -> DTO
+    public CommentResponse toDto(Comment reply) {
+        return CommentResponse.toDto(
+                reply.getId(),
+                reply.getContent(),
+                reply.getUser().getNickname(),
+                reply.getLikes(),
+                reply.getCreatedAt(),
+                reply.getUpdatedAt());
+    }
+
+    // 대댓글 리스트 변환
+    public List<CommentResponse> toDto(List<Comment> replies) {
+        return replies.stream()
+                .map(this::toDto)
+                .toList();
     }
 }
