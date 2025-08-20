ccba93f7b4ab5e9c3c5692dc1aec4af5c790b688
Over-night <hidden98716@gmail.com>
2025-08-20 12:01:14 +0900

    feat(USR-SER): 기본 유저 정보 관련 Service & Controller 구현

--- src/main/java/com/spartaboys/newsfeed/domain/users/service/UserService.java
+++ src/main/java/com/spartaboys/newsfeed/domain/users/service/UserService.java
@@ -1,17 +1,70 @@
 package com.spartaboys.newsfeed.domain.users.service;
 
+import com.spartaboys.newsfeed.domain.users.dto.request.ChangePasswordRequest;
+import com.spartaboys.newsfeed.domain.users.dto.request.UserUpdateRequest;
+import com.spartaboys.newsfeed.domain.users.dto.response.UserPrivateResponse;
+import com.spartaboys.newsfeed.domain.users.dto.response.UserUpdateResponse;
+import com.spartaboys.newsfeed.domain.users.mapper.UserMapper;
 import com.spartaboys.newsfeed.domain.users.dto.response.UserPublicResponse;
+import com.spartaboys.newsfeed.domain.users.entity.User;
+import com.spartaboys.newsfeed.domain.users.exception.InvalidUserException;
+import com.spartaboys.newsfeed.domain.users.exception.UserErrorCode;
 import com.spartaboys.newsfeed.domain.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class UserService {
-    private UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final UserRepository userRepository;
 
-    private UserPublicResponse getUserById
+    private User getUserOrThrow(Long id) {
+        return userRepository.findByIdAndDeletedIsFalse(id)
+                .orElseThrow(() -> new InvalidUserException(UserErrorCode.USR_INVALID_ID));
+    }
+    private <T> T getUserMapped(Long id, Function<User, T> mapper) {
+        User user = getUserOrThrow(id);
+
+        return mapper.apply(user);
+    }
+
+    public UserPublicResponse getPublicUserById(Long id) {
+        return getUserMapped(id, userMapper::toPublicResponse);
+    }
+
+    public UserPrivateResponse getPrivateUserById(Long id) {
+        return getUserMapped(id, userMapper::toPrivateResponse);
+    }
+
+    public UserUpdateResponse updateUserProfile(Long id, UserUpdateRequest dto) {
+        if (userRepository.existsByNickname(dto.getNickname())) {
+            throw new InvalidUserException(UserErrorCode.USR_DUPLICATE_EMAIL);
+        }
+
+        User currentUser = getUserOrThrow(id);
+        currentUser.updateNickname(dto.getNickname());
+
+        return userMapper.toUpdateResponse(currentUser);
+    }
+
+    public void updateUserPassword(Long id, ChangePasswordRequest dto) {
+        // TODO : 비밀번호 암호화 로직 완성되는대로 반영
+        if (dto.getNewPasswordConfirm().equals(dto.getNewPassword())) {
+            throw new InvalidUserException(UserErrorCode.USR_PW_CHECK_MISMATCH);
+        }
+
+        User currentUser = getUserOrThrow(id);
+        if (!currentUser.getPassword().equals(dto.getCurrentPassword())) {
+            throw new InvalidUserException(UserErrorCode.USR_PW_CURRENT_MISMATCH);
+        }
+
+        currentUser.updatePassword(dto.getNewPassword());
+    }
 
 }
