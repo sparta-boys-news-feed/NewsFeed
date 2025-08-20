5a483060e98306f9a2be57c7b165ea7908bc9fd5
Over-night <hidden98716@gmail.com>
2025-08-20 10:44:26 +0900

    feat(USR-REP): JPA Repository 상속추가

--- src/main/java/com/spartaboys/newsfeed/domain/users/repository/UserRepository.java
+++ src/main/java/com/spartaboys/newsfeed/domain/users/repository/UserRepository.java
@@ -1,13 +1,12 @@
 package com.spartaboys.newsfeed.domain.users.repository;
 
 import com.spartaboys.newsfeed.domain.users.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository {
-    Optional<User> findById(Long id);
-
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByNickname(String username);
