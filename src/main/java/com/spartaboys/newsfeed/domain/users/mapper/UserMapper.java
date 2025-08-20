7c12d2478463551d178ed74f0c26e06b93ee6329
Over-night <hidden98716@gmail.com>
2025-08-20 12:01:14 +0900

    feat(USR-SER): 기본 유저 정보 관련 Service & Controller 구현

--- src/main/java/com/spartaboys/newsfeed/domain/users/mapper/UserMapper.java
+++ src/main/java/com/spartaboys/newsfeed/domain/users/mapper/UserMapper.java
@@ -1,7 +1,9 @@
 package com.spartaboys.newsfeed.domain.users.mapper;
 
 import com.spartaboys.newsfeed.common.exception.ErrorCode;
+import com.spartaboys.newsfeed.domain.users.dto.response.UserPrivateResponse;
 import com.spartaboys.newsfeed.domain.users.dto.response.UserPublicResponse;
+import com.spartaboys.newsfeed.domain.users.dto.response.UserUpdateResponse;
 import com.spartaboys.newsfeed.domain.users.entity.User;
 import org.springframework.stereotype.Component;
 
@@ -10,4 +12,6 @@ public class UserMapper {
     public UserPublicResponse toPublicResponse(User user) {
         return UserPublicResponse.of(user.getId(), user.getNickname());
     }
+    public UserPrivateResponse toPrivateResponse(User user) { return UserPrivateResponse.of(user.getId(), user.getEmail(), user.getNickname(), user.getCreatedAt(), user.getModifiedAt()); }
+    public UserUpdateResponse toUpdateResponse(User user) { return UserUpdateResponse.of(user.getId(), user.getNickname(), user.getModifiedAt()); }
 }
