0e22a6c2d9505d348dac10d6d83f87a8f7025f95
MinHyeongK <syjjmh1@naver.com>
2025-08-20 15:49:27 +0900

    refactor(board): convert DTO class to record

--- src/main/java/com/spartaboys/newsfeed/domain/boards/dto/request/BoardRequest.java
+++ src/main/java/com/spartaboys/newsfeed/domain/boards/dto/request/BoardRequest.java
@@ -2,16 +2,11 @@ package com.spartaboys.newsfeed.domain.boards.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
-public class BoardRequest {
-    @NotBlank(message = "제목을 입력해주세요.")
-    @Size(min = 2, max = 50, message = "제목은 2자 이상 50자 이하로 입력해주세요.")
-    String title;
-    @NotBlank(message = "내용을 입력해주세요.")
-    @Size(min = 2, max = 1000, message = "제목은 2자 이상 1000자 이하로 입력해주세요.")
-    String content;
+public record BoardRequest(@NotBlank(message = "제목을 입력해주세요.")
+                           @Size(min = 2, max = 50, message = "제목은 2자 이상 50자 이하로 입력해주세요.")
+                           String title,
+                           @NotBlank(message = "내용을 입력해주세요.")
+                           @Size(min = 2, max = 1000, message = "제목은 2자 이상 1000자 이하로 입력해주세요.")
+                           String content) {
 }
