0a451c5998c390489a5e640f41989c365d71c12b
seyoung5744 <seyoung574458@gmail.com>
2025-08-20 21:48:15 +0900

    feat(following) : 유저 언팔로우

--- src/main/java/com/spartaboys/newsfeed/domain/follow/controller/FollowController.java
+++ src/main/java/com/spartaboys/newsfeed/domain/follow/controller/FollowController.java
@@ -1,7 +1,7 @@
 package com.spartaboys.newsfeed.domain.follow.controller;
 
 import com.spartaboys.newsfeed.common.response.ApiResponse;
-import com.spartaboys.newsfeed.domain.follow.service.FollowQueryService;
+import com.spartaboys.newsfeed.domain.follow.service.FollowCommandService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,14 +11,27 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class FollowController {
 
-    private final FollowQueryService followQueryService;
+    private final FollowCommandService followCommandService;
 
     @PostMapping("/following/{followeeId}")
     public ResponseEntity<ApiResponse<Void>> followUser(
             @SessionAttribute(name = "login_id") Long followerId,
             @PathVariable Long followeeId
     ) {
-        followQueryService.followUser(followerId, followeeId);
+        followCommandService.followUser(followerId, followeeId);
         return ApiResponse.noContent();
     }
+
+    @DeleteMapping("/following/{followeeId}")
+    public ResponseEntity<ApiResponse<Void>> unFollowUser(
+            @SessionAttribute(name = "login_id") Long followerId,
+            @PathVariable Long followeeId
+    ) {
+        followCommandService.unFollowUser(followerId, followeeId);
+        return ApiResponse.noContent();
+    }
+
+//    @GetMapping("/following")
+//    public ResponseEntity<ApiResponse<?>> getFollowingList(@SessionAttribute(name = "login_id") Long loginId) {
+//    }
 }
