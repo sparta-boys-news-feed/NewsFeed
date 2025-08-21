f2cbb30d117003cc8e1a192fa85e6e43b7be53d6
seyoung5744 <seyoung574458@gmail.com>
2025-08-21 12:36:11 +0900

    FollowErrorCode 충돌 해결

--- src/main/java/com/spartaboys/newsfeed/domain/follow/exception/FollowErrorCode.java
+++ src/main/java/com/spartaboys/newsfeed/domain/follow/exception/FollowErrorCode.java
@@ -13,7 +13,9 @@ public enum FollowErrorCode implements ErrorCode {
     SELF_UNFOLLOW_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "자기 자신은 언팔로우할 수 없습니다."),
 
     ALREADY_FOLLOWING(HttpStatus.CONFLICT, "이미 팔로우한 사용자입니다."),
-    ALREADY_UNFOLLOWING(HttpStatus.CONFLICT, "이미 언팔로우한 사용자입니다.");
+    ALREADY_UNFOLLOWING(HttpStatus.CONFLICT, "이미 언팔로우한 사용자입니다."),
+
+    FOLLOW_NOT_FOUND(HttpStatus.NOT_FOUND, "팔로우 관계를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
