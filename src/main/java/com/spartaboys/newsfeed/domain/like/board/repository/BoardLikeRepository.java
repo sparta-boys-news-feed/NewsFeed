71e058bd4b5d34fb3f53484b23d3921589ff393f
seyoung5744 <seyoung574458@gmail.com>
2025-08-22 19:23:54 +0900

    feat(like) : like internal service 추가

--- src/main/java/com/spartaboys/newsfeed/domain/like/board/repository/BoardLikeRepository.java
+++ src/main/java/com/spartaboys/newsfeed/domain/like/board/repository/BoardLikeRepository.java
@@ -3,8 +3,11 @@ package com.spartaboys.newsfeed.domain.like.board.repository;
 import com.spartaboys.newsfeed.domain.boards.entity.Board;
 import com.spartaboys.newsfeed.domain.like.entity.BoardLike;
 import com.spartaboys.newsfeed.domain.users.entity.User;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
+import java.util.List;
 import java.util.Optional;
 
 public interface BoardLikeRepository extends JpaRepository<BoardLike, Long> {
@@ -13,4 +16,7 @@ public interface BoardLikeRepository extends JpaRepository<BoardLike, Long> {
 
     Optional<BoardLike> findByUserAndBoard(User user, Board board);
 
+    List<BoardLike> findAllByUser(User user);
+
+    Page<BoardLike> findAllByUser(User user, Pageable pageable);
 }
