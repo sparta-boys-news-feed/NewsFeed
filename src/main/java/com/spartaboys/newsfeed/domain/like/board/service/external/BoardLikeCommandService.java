ee03b0e251de78795d943cb1ee8823c77cbdee99
seyoung5744 <seyoung574458@gmail.com>
2025-08-22 19:23:54 +0900

    feat(like) : like internal service 추가

--- src/main/java/com/spartaboys/newsfeed/domain/like/board/service/external/BoardLikeCommandService.java
+++ src/main/java/com/spartaboys/newsfeed/domain/like/board/service/external/BoardLikeCommandService.java
@@ -1,4 +1,4 @@
-package com.spartaboys.newsfeed.domain.like.board.service;
+package com.spartaboys.newsfeed.domain.like.board.service.external;
 
 import com.spartaboys.newsfeed.domain.boards.entity.Board;
 import com.spartaboys.newsfeed.domain.boards.service.BoardInternalService;
