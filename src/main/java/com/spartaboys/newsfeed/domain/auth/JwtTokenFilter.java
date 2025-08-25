011d09dcc1de63c9b591b3f21a474271696139c5
sonomoo <sbxjs010@naver.com>
2025-08-25 20:55:05 +0900

    feat(auth): jwt token을 컨트롤러까지 전달하기위한 필터개발 - jwt 필터를 개발하기 위한 바탕

--- src/main/java/com/spartaboys/newsfeed/domain/auth/JwtTokenFilter.java
+++ src/main/java/com/spartaboys/newsfeed/domain/auth/JwtTokenFilter.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -16,6 +17,7 @@ import java.io.IOException;
 //OncePerRequestFilter
 @FilterRegistration
 @RequiredArgsConstructor
+@Component
 public class JwtTokenFilter extends OncePerRequestFilter {
 
     private final AccessService accessService;
